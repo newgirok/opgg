f33e4ab94aeb499c21b437065b25c8c53d13f7a3
henry <henry@crm.co.kr>
2026-06-23 10:46:38 +0900

    feat(server): 소환사 정보 조회 API (puuid 기반 summoner 정보, GET /user)

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,4 +12,40 @@ router.use((req, res, next) => {
   next();
 });
 
+const getSummonerInfo = async (gameName, tagLine) => {
+  try {
+    const accountResponse = await axios.get(
+      `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`,
+      {
+        headers: {
+          "X-Riot-Token": RIOT_API_KEY,
+        },
+      }
+    );
+
+    const puuid = accountResponse.data.puuid;
+
+    const summonerResponse = await axios.get(
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+      {
+        headers: {
+          "X-Riot-Token": RIOT_API_KEY,
+        },
+      }
+    );
+
+    return summonerResponse.data;
+  } catch (error) {
+    console.error(
+      "Error fetching data:",
+      error.response ? error.response.data : error.message
+    );
+  }
+};
+
+router.get("/", async (req, res) => {
+  let userInfo = await getSummonerInfo(res.locals.name, res.locals.tag);
+  res.json(userInfo);
+});
+
 module.exports = router;
